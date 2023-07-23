3701f55441c540897424da163cfc44aa5b8a8623
jarno9981 <53493418+jarno9981@users.noreply.github.com>
2023-07-23 14:19:07 +0300

    Delete bluebirdTransFolder directory
    Remove transfolder not in use

--- bluebirdTransFolder/Bluebird/Bluebird/Core/FavoritesHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Bluebird.Core;
-
-public class FavoritesHelper
-{
-    public static void AddFavoritesItem(string title, string url)
-    {
-        Json.AddItemToJson("Favorites.json", title, url);
-    }
-}
